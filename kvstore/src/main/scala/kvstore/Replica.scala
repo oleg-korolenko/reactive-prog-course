8bf396a5262236f4b6104a8f8c84203f478148f6
Oleg Korolenko <oleg.korolenko@bull.be>
2013-12-16 09:00:37 +0100

    week6 : First & 2nd steps

--- kvstore/src/main/scala/kvstore/Replica.scala
+++ kvstore/src/main/scala/kvstore/Replica.scala
@@ -1,11 +1,11 @@
 package kvstore
 
-import akka.actor.{ OneForOneStrategy, Props, ActorRef, Actor }
+import akka.actor.{OneForOneStrategy, Props, ActorRef, Actor}
 import kvstore.Arbiter._
 import scala.collection.immutable.Queue
 import akka.actor.SupervisorStrategy.Restart
 import scala.annotation.tailrec
-import akka.pattern.{ ask, pipe }
+import akka.pattern.{ask, pipe}
 import akka.actor.Terminated
 import scala.concurrent.duration._
 import akka.actor.PoisonPill
@@ -14,23 +14,32 @@ import akka.actor.SupervisorStrategy
 import akka.util.Timeout
 
 object Replica {
+
   sealed trait Operation {
     def key: String
+
     def id: Long
   }
+
   case class Insert(key: String, value: String, id: Long) extends Operation
+
   case class Remove(key: String, id: Long) extends Operation
+
   case class Get(key: String, id: Long) extends Operation
 
   sealed trait OperationReply
+
   case class OperationAck(id: Long) extends OperationReply
+
   case class OperationFailed(id: Long) extends OperationReply
+
   case class GetResult(key: String, valueOption: Option[String], id: Long) extends OperationReply
 
   def props(arbiter: ActorRef, persistenceProps: Props): Props = Props(new Replica(arbiter, persistenceProps))
 }
 
 class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
+
   import Replica._
   import Replicator._
   import Persistence._
@@ -39,25 +48,67 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
    */
-  
+
   var kv = Map.empty[String, String]
   // a map from secondary replicas to replicators
   var secondaries = Map.empty[ActorRef, ActorRef]
   // the current set of replicators
   var replicators = Set.empty[ActorRef]
 
+  var seqNumber = 0
+
+  //sending join message back to arbiter to register as Primary/Secondary replica
+  arbiter ! Join
+
   def receive = {
-    case JoinedPrimary   => context.become(leader)
+    case JoinedPrimary => context.become(leader)
     case JoinedSecondary => context.become(replica)
   }
 
   /* TODO Behavior for  the leader role. */
   val leader: Receive = {
+    case Insert(key, value, id) => {
+      kv += (key -> value)
+      sender ! OperationAck(id)
+    }
+    case Get(key, id) => {
+      sender ! GetResult(key, kv.get(key), id)
+    }
+    case Remove(key, id) => {
+      kv -= (key)
+      sender ! OperationAck(id)
+    }
+    case Replicas(replicas) => {
+      //TODO check all replicas , add/remove
+      ???
+
+    }
     case _ =>
   }
 
   /* TODO Behavior for the replica role. */
   val replica: Receive = {
+    case Get(key, id) => {
+      sender ! GetResult(key, kv.get(key), id)
+    }
+    case Snapshot(key, value, seq) => {
+      if (seq == seqNumber) {
+        seqNumber += 1
+        value match {
+          case Some(s) => {
+            kv += (key -> s)
+            sender ! SnapshotAck(key, seq)
+          }
+          case None => {
+            kv -= (key)
+            sender ! SnapshotAck(key, seq)
+          }
+        }
+      }
+      else if (seq < seqNumber) sender ! SnapshotAck(key, seq)
+      //else we drop message
+
+    }
     case _ =>
   }
 
--- kvstore/src/test/scala/kvstore/Step3_ReplicatorSpec.scala
+++ kvstore/src/test/scala/kvstore/Step3_ReplicatorSpec.scala
@@ -1,21 +1,21 @@
 package kvstore
 
-import akka.testkit.{ TestProbe, TestKit, ImplicitSender }
+import akka.testkit.{TestProbe, TestKit, ImplicitSender}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FunSuite
 import akka.actor.ActorSystem
 import scala.concurrent.duration._
-import kvstore.Arbiter.{ JoinedSecondary, Join }
-import kvstore.Persistence.{ Persisted, Persist }
-import kvstore.Replicator.{ SnapshotAck, Snapshot, Replicate }
+import kvstore.Arbiter.{JoinedSecondary, Join}
+import kvstore.Persistence.{Persisted, Persist}
+import kvstore.Replicator.{SnapshotAck, Snapshot, Replicate}
 
 class Step3_ReplicatorSpec extends TestKit(ActorSystem("Step3ReplicatorSpec"))
-  with FunSuite
-  with BeforeAndAfterAll
-  with ShouldMatchers
-  with ImplicitSender
-  with Tools {
+with FunSuite
+with BeforeAndAfterAll
+with ShouldMatchers
+with ImplicitSender
+with Tools {
 
   override def afterAll(): Unit = {
     system.shutdown()
@@ -27,17 +27,23 @@ class Step3_ReplicatorSpec extends TestKit(ActorSystem("Step3ReplicatorSpec"))
 
     replicator ! Replicate("k1", Some("v1"), 0L)
     secondary.expectMsg(Snapshot("k1", Some("v1"), 0L))
-    secondary.ignoreMsg({ case Snapshot(_, _, 0L) => true })
+    secondary.ignoreMsg({
+      case Snapshot(_, _, 0L) => true
+    })
     secondary.reply(SnapshotAck("k1", 0L))
 
     replicator ! Replicate("k1", Some("v2"), 1L)
     secondary.expectMsg(Snapshot("k1", Some("v2"), 1L))
-    secondary.ignoreMsg({ case Snapshot(_, _, 1L) => true })
+    secondary.ignoreMsg({
+      case Snapshot(_, _, 1L) => true
+    })
     secondary.reply(SnapshotAck("k1", 1L))
 
     replicator ! Replicate("k2", Some("v1"), 2L)
     secondary.expectMsg(Snapshot("k2", Some("v1"), 2L))
-    secondary.ignoreMsg({ case Snapshot(_, _, 2L) => true })
+    secondary.ignoreMsg({
+      case Snapshot(_, _, 2L) => true
+    })
     secondary.reply(SnapshotAck("k2", 2L))
 
     replicator ! Replicate("k1", None, 3L)
@@ -55,6 +61,20 @@ class Step3_ReplicatorSpec extends TestKit(ActorSystem("Step3ReplicatorSpec"))
     secondary.expectMsg(300.milliseconds, Snapshot("k1", Some("v1"), 0L))
 
     secondary.reply(SnapshotAck("k1", 0L))
+  }
+
+  test("replicated :Replicator should send back replicated messsage") {
+    val primary = TestProbe()
+    val secondary = TestProbe()
+    val replicator = system.actorOf(Replicator.props(secondary.ref), "case2-replicator")
+
+    replicator ! Replicate("k1", Some("v1"), 0L)
+    secondary.expectMsg(Snapshot("k1", Some("v1"), 0L))
+    secondary.ignoreMsg({
+      case Snapshot(_, _, 0L) => true
+    })
+    secondary.reply(SnapshotAck("k1", 0L))
+
   }
 
 }
